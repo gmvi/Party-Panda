b3c52d467bae7abb65116fd3fbc9604d88dadc48
George Matter VI <george.matter.vi@gmail.com>
2014-03-25 01:42:32 -0700

    server starts again\! The end of the refactor is near\!

--- server/PluginLoader.js
+++ server/PluginLoader.js
@@ -1,11 +1,14 @@
 var fs = require('fs');
 // All this just to remove an unwieldly if/else block
-module.exports = function PluginsLoader(dirname, plugin_id, required)
-{ if (typeof(dirname) != 'string')
+// TODO hardwire this into the app when I add more OAuth providers ?
+module.exports = function PluginsLoader(dirname, options)
+{ var plugin_id = options.id || 'id';
+  var required = options.required || [];
+  var default_plugin = options['default'] || null;
+  if (typeof(dirname) != 'string')
     throw new TypeError("dirname must be a string");
   if (typeof(plugin_id) != 'string')
-    throw new TypeError("plugin_id must be a string");
-  if (!required) required = [];
+    throw new TypeError("options.id must be a string");
   var plugins = {}; // hash to hold plugin objects
   this.reload = function reload() // load plugins from the specified directory
   { var dir = fs.readdirSync(dirname);
@@ -16,8 +19,8 @@ module.exports = function PluginsLoader(dirname, plugin_id, required)
       var ends_js = dir[i].substring(dir[i].length-3) == '.js';
       var stats = fs.statSync(path_i_);
       if (stats.isDirectory())
-      { // If a child of the specified directory is a directory, check inside it for a file called
-        // plugin.js
+      { // If a child of the specified directory is a directory, check inside
+        // it for a file called plugin.js
         if (fs.existsSync(path_i_+"/plugin.js"))
         { dir[i] = "plugin.js"; // kinda hacky but it works
           path_i_ = path_i_+"/plugin.js";
@@ -29,7 +32,7 @@ module.exports = function PluginsLoader(dirname, plugin_id, required)
       { // Before requiring the plugin, we must first invalidate any entry in
         // the cache it may have.
         delete require.cache[require.resolve('./'+path_i_)];
-        plug = require('./'+path_i_); // new require(path_i_, storage) ?
+        plug = require('./'+path_i_);
         // check loaded plugin for conformity to given specs.
         var missing_attrs = [];
         if (!plug[plugin_id])
@@ -54,8 +57,13 @@ module.exports = function PluginsLoader(dirname, plugin_id, required)
     }
   }
   this.reload();
+  if (!(default_plugin in plugins))
+    default_plugin = null;
   this.get = function get(id)
-  { return plugins[id];
+  { id = id || default_plugin;
+    if (id == null)
+      throw new Error("couldn't find plugin named " + id + " and no default exists");
+    return plugins[id];
   };
   this.all = function all()
   { return Object.keys(plugins);
--- server/app.js
+++ server/app.js
@@ -1,152 +1,231 @@
-// Party Panda
-// A controller which mediates a pandora playlist based on the votes of a
-// group of people.
+/* Party Panda
+ *
+ * A controller which mediates a pandora playlist based on the votes of a group
+ * of people.
+ */
+
+
+//* IMPORTS
 
 var fs = require('fs'),
     ws = require('ws'),
-    util = require('util'),
     http = require('http'),
     jade = require('jade'),
     redis = require('redis'),
     express = require('express'),
-    fibrous = require('fibrous'), // can I remove this and replace with promises?
-    Promise = require('es6-promise').Promise,
+    fakeredis = require('fakeredis');
+
+var Promise = require('es6-promise').Promise,
     PluginLoader = require('./PluginLoader.js'),
-    MemoryDatabase = require('./database/MemoryDatabase.js'),
-    RedisDatabase = require('./database/RedisDatabase.js');
-var RedisStore = require('connect-redis')(express);
+    DatabaseController = require('./DatabaseController.js');
+
 var MemoryStore = express.session.MemoryStore;
+var RedisStore = require('connect-redis')(express);
 
-var usersystemPlugins = new PluginLoader("plugins", "name");
+//** VARIOUS SETUP AND UTILS
 
-// LOAD SETTINGS
+var usersystems = new PluginLoader("usersystems", {id: "name"});
+
+function std_catch(res)
+{ return function(err)
+  { console.log(err.stack);
+    if (res != undefined) res.send(500);
+  }
+}
+
+
+//** STANDARD TYPE PROTOTYPE EXTENSIONS
+
+// string.format replaces `{i}` in a string with the ith argument, if it exists
+if (!String.prototype.format)
+{ String.prototype.format = function format()
+  { var args = arguments; // capture the arguments to format()
+    return this.replace(/{(\d+)}/g, function(match, number)
+    { if (typeof args[number] != 'undefined')
+        return args[number];
+      else return match;
+    });
+  };
+}
+
+
+//** SETTINGS
+
 var settings;
-if (fs.existsSync(__dirname + "/settings.json"))
+try
 { settings = require("./settings.json");
 }
-else
+catch (err)
 { console.info("Warning: using default settings file");
   settings = require("./settings-default.json");
 }
 
-// DATABASE CONTROLLER
-var database;
+
+//** DATABASE CONTROLLER
+var client;
+var sessionStore;
 if (settings.debug)
-  database = new MemoryDatabase();
+{ client = fakeredis.createClient();
+  sessionStore = new MemoryStore();
+}
 else
-{ var redisClient = redis.createClient();
-  database = new RedisDatabase(redisClient);
+{ client = redis.createClient();
+  sessionStore = new RedisStore({'client': client});
 }
 
-default_room_settings = { session : "session",
-                          minVotes : settings['min votes'],
-                          thresholdUp : settings['upvote threshold'],
-                          thresholdDown : settings['downvote threshold']
-                        };
+default_settings = { usersystem : "session",
+                     minVotes : settings['min votes'],
+                     thresholdUp : settings['upvote threshold'],
+                     thresholdDown : settings['downvote threshold']
+                   };
 
+var database = new DatabaseController({client:           client,
+                                       default_settings: default_settings});
+
+// TODO: re-evalutate after switch to redis's pub-sub
 var registerVote = function registerVote(room, unique, vote)
-{ // ensure unique identifier exists
-  var db_call = database.setVote(room, unique, vote);
-  return db_call.then(function(upvotes, downvotes)
-  { var total = upvotes+downvotes;
-    settings_promise = database.getSettings(room, ['minVotes',
-                                                   'thresholdUp',
-                                                   'thresholdDown']);
-    return settings_promise.then(function(room_settings)
+{ return database.storeVote(room, unique, vote).then(function(votes)
+  { var total = votes.upvotes + votes.downvotes;
+
+    return database.getSettings(room, ['minVotes',
+                                       'thresholdUp',
+                                       'thresholdDown'])
+      .then(function(room_settings)
     { if (total > room_settings.minVotes)
-      { if (up/total >= room_settings.thresholdUp)
-          wsclient.emitVote('up');
-        else if (down/total <= room_settings.thresholdDown)
-          wsclient.emitVote('down');
+      { if (votes.upvotes/total >= room_settings.thresholdUp)
+          wsClients.emitVote('up');
+        else if (votes.downvotes/total <= room_settings.thresholdDown)
+          wsClients.emitVote('down');
       }
     });
-  }).error(function(err)
-  { console.log("error registering vote from '"+unique+"' in room '"+room+"'");
+  }).catch(function(err)
+  { console.log("error registering vote from {0} in room {1}: {2}"
+                .format(unique, room, vote));
+    console.log(err);
     throw err;
   });
 }
 
 
-// SERVER AND EXPRESS SETUP
+//** TEMPLATES
+function render(template, variables)
+{ var path = __dirname + "/templates/{0}.jade".format(template);
+  return jade.renderFile(path, variables);
+}
+
+
+//** SERVER AND EXPRESS SETUP
 var app = new express();
 var server = http.createServer(app);
 var wsServer = new ws.Server({server: server});
 
-var cookieParser = new express.cookieParser(settings['session secret']);
-var sessionStore;
-if (settings.debug)
-  sessionStore = new MemoryStore();
-else
-var sessionParser = new express.session({ key: "express.sid",
-                                          // secret: settings['session secret'],
-                                          store: sessionStore });
-//app.use(express.logger());
+var cookieParser = express.cookieParser(settings['cookie secret']);
+var sessionParser = express.session({ key: "express.sid",
+                                      store: sessionStore });
 app.use(cookieParser);
 app.use(sessionParser);
 
 // static folders and files
-app.use('/styles', express.static(__dirname + '/styles')); // local css
-app.use('/scripts', express.static(__dirname + '/scripts')); // local js
-app.use('/assets', express.static(__dirname + '/assets')); // images etc
-app.use('/bower', express.static(__dirname + '/bower_components')); // bower components)
+app.use('/styles',  express.static(__dirname + '/styles'));
+app.use('/scripts', express.static(__dirname + '/scripts'));
+app.use('/assets',  express.static(__dirname + '/assets'));
+app.use('/bower',   express.static(__dirname + '/bower_components'));
 
 
-// APP ENDPOINTS
+//** APP ENDPOINTS
 app.get('/', function (req, res)
-{ function send_page(req, res)
-  { vote = database.getVote(null, req.session.token);
-    res.send(jade.renderFile(__dirname + "/templates/index.jade", {"vote": vote}));
-  }
-  if (usersystem.is_logged_in(req.session))
-  { send_page(req, res);
-  }
-  else;
-  { usersystem.request_login(req, res, "http://"+req.headers.host+"/login");
-  }
+{ database.getSetting("null", "usersystem").then(function(usersystem_name)
+  { usersystem = usersystems.get(usersystem_name);
+    if (usersystem.is_logged_in(req.session))
+    { database.getVote("null", req.session.token).then(function(vote)
+      { res.send(render("index", {"vote": vote}));
+      }).catch(std_catch(res));
+    }
+    else
+    { var redirect = "http://"+req.headers.host+"/login";
+      usersystem.request_login(req, res, redirect);
+    }
+  }).catch(std_catch(res));
 });
 app.get('/login', function (req, res)
-{ // TODO: verify code by accessing user's name on codeday.org
-  if (usersystem.accept_login(req))
-  { console.log("logging in");
-    res.redirect('/');
-  }
-  else
-  { console.log("failed login attempt");
-    res.redirect('/error?error=login');
-  }
+{ database.getSetting("null", "usersystem").then(function(usersystem_name)
+  { var usersystem = usersystems.get(usersystem_name);
+    if (usersystem.accept_login(req))
+    { console.log("logging in");
+      res.redirect('/');
+    }
+    else
+    { console.log("failed login attempt");
+      res.redirect('/error?error=login');
+    }
+  }).catch(std_catch(res));
 });
-// API in place of websocket interface just in case
+app.get('/error', function(req, res)
+{ res.send(500);
+});
+
+// HTTP API as well as websocket interface just in case
 app.post('/vote/down', function (req, res)
-{ if (usersystem.is_logged_in(req.session))
-  { registerVote('main', req.session.token, 'down').then(function ()
-    { res.send(200);
-    });
-  }
-  else res.send(403);
+{ database.getSetting("null", "usersystem").then(function(usersystem_name)
+  { var usersystem = usersystems.get(usersystem_name);
+    if (usersystem.is_logged_in(req.session))
+    { registerVote('null', req.session.token, 'down')
+        .then(res.send.bind(res, 200))
+        .catch(res.send.bind(res, 500));
+    }
+    else res.send(403);
+  }).catch(std_catch(res));
 });
 app.post('/vote/up', function (req, res)
 { if (usersystem.is_logged_in(req.session))
-  { registerVote('main', req.session.token, 'up').then(function ()
-    { res.send(200);
-    });
+  { registerVote('null', req.session.token, 'up')
+      .then(res.send.bind(res, 200))
+      .catch(res.send.bind(res, 500));
   }
   else res.send(403);
 });
 
-// TODO: reevalute the following for shift to multi-room system
 
-// WEBSOCKET LOGIC
+//** WEBSOCKET LOGIC
+
+// object to handle broadcasting to connected clients
+var wsClients = new function Clients()
+{ rooms = Object.create(null);
+  function get(room)
+  { if (!(room in rooms))
+      rooms[room] = Object.create(null);
+    return rooms[room];
+  }
+  this.add = function add(room, ws)
+  { room = get(room);
+    room[ws.upgradeReq.headers['sec-websocket-key']] = ws;
+  }
+  this.remove = function remove(room, ws)
+  { room = get(room);
+    delete room[ws.upgradeReq.headers['sec-websocket-key']];
+  }
+  this.broadcast = function broadcast(room, message)
+  { room = get(room);
+    for (key in room)
+      room[key].send(message);
+  }
+  this.emitVote = function emitVote(room, vote)
+  { this.broadcast(room, "consensus:"+vote);
+  }
+}();
+
+// parser function for websocket messages
 function parse(message)
 { var type;
   var data;
-  if (1 + message.indexOf(":"))
-  { message = message.split(":");
+  var i = message.indexOf(":");
+  if (1 + i)
+  { type = message.substring(0, i);
+    data = message.substring(i+1);
     try
-    { message[1] = JSON.parse(message[1]);
+    { data = JSON.parse(data);
     }
-    catch (SyntaxError) { }
-    type = message[0];
-    data = message[1];
+    catch (err) { /* body is not valid JSON */ }
   }
   else
   { type = "message";
@@ -157,92 +236,83 @@ function parse(message)
          };
 }
 
-// how do I do sessions with extension web sockets?
-/// I register a room when I get an extension room creation request
-/// I unregister a room when I lose an extension websocket
-//// future: extension websocket heartbeats and timeout?
-/// the redis database records the set of taken rooms
-var wsPandoras = new function Pandoras()
-{ clients = Object.create(null);
-  this.add = function add(ws)
-  { clients[ws.upgradeReq.headers['sec-websocket-key']] = ws;
-  }
-  this.remove = function remove(ws)
-  { delete clients[ws.upgradeReq.headers['sec-websocket-key']];
-  }
-  this.broadcast = function broadcast(message)
-  { for (key in clients) clients[key].send(message);
-  }
-}();
-var wsClients = new function Clients()
-{ clients = Object.create(null);
-  this.add = function add(ws)
-  { clients[ws.upgradeReq.headers['sec-websocket-key']] = ws;
-  }
-  this.remove = function remove(ws)
-  { delete clients[ws.upgradeReq.headers['sec-websocket-key']];
-  }
-  this.broadcast = function broadcast(message)
-  { for (key in clients) clients[key].send(message);
-  }
-  this.emitVote = function emitVote(vote)
-  { }
-}();
+// TODO: extension websocket heartbeats and timeout?
+// the redis database records the set of taken rooms
+// for scaling past one process, we will use redis's pub-sub system
 
-function is_connection_from_extension(origin)
+
+function is_from_extension(origin)
 { if (origin == "http://www.pandora.com") return true;
-  origin = origin.split("//");
-  return ( origin[0] == "chrome-extension:" &&
-           ! ( settings['restrict chrome extension'] &&
-               origin[1] != settings['restrict chrome extension']
-             )
-         );
-  // ^- obtuse way to say "if origin is an acceptable chome-extension (test) url"
+  if (settings.debug)
+  { var protocol = origin.split("://")[0];
+    return protocol == "chrome-extension";
+  }
+  return false;
 }
 
 wsServer.on('connection', function(ws) {
-  if (is_connection_from_extension(ws.upgradeReq.headers.origin)) // extension
-  { console.log("got connection from extension");
-    ws.on('message', function(message) {
-      message = parse(message);
-      if (message.type == "update")
-      { database.resetVotes(null);
-        wsClients.broadcast(message.originalMessage);
-      }
-      else
-        console.log("extraneous message from extension: " + JSON.stringify(message));
-    });
-    if (extensionWS) extensionWS.terminate();
-    extensionWS = ws;
-    database.resetVotes(null);
-  }
-  else // client
-  { // TODO: reevaluate, maybe use Promises instead of fibrous?
-    //       Why did I even use fibrous in the first place
-    fibrous.run(function() // attatch session to socket
-    { cookieParser.sync(ws.upgradeReq, null);
+  new Promise(function(resolve, reject) // attatch session to socket
+  { cookieParser(ws.upgradeReq, null, function(err)
+    { if (err) return reject(err);
       var sessionID = ws.upgradeReq.signedCookies['express.sid'];
-      session = sessionstore.sync.load(sessionID);
-      ws.session = session;
+      sessionStore.load(sessionID, function(err, session)
+      { if (err) return reject(err);
+        ws.session = session;
+        resolve();
+      });
     });
-    ws.on('message', function(message)
-    { message = parse(message);
-      if (message.type == "vote")
-      { registerVote(null, ws.session, message.data)
-      }
-      // else if (message == "test")
-      // { ws.send("session:"+JSON.stringify(ws.session));
-      // }
-      else
-        console.log("extraneous message from client: "+JSON.stringify(message));
-    });
-    ws.on('close', function()
-    { wsClients.remove(ws);
-    });
-    // store websocket for broadcasting to it.
-    wsClients.add(ws);
-  }
+  }).then(function()
+  { var from_extension = is_from_extension(ws.upgradeReq.headers.origin);
+    if (from_extension)
+    { console.log("got connection from extension");
+      ws.on('message', function(message)
+      { var orig_message = message;
+        message = parse(message);
+        if (message.type == "start")
+        { message.data
+        }
+        else if (message.type == "new_song")
+        { // TODO: handle song update
+          database.resetVotes("null")
+                  .catch(std_catch());
+          wsClients.broadcast(orig_message);
+        }
+        else if (message.type == "close")
+        { // TODO: handle room close
+        }
+        else
+          console.log("extraneous message from extension: "
+                       + orig_message);
+      });
+      ws.send("ready");
+    }
+    else // from web client
+    { ws.on('message', function(message)
+      { parsed = parse(message);
+        if (parsed.type == "vote")
+        { if (parsed.data != "up" &&
+              parsed.data != "down")
+            console.log("bad vote from web client: {0}".format(parsed.data));
+          else
+            registerVote("null", ws.session, parsed.data)
+        }
+        else
+          console.log("extraneous message from web client: `{0}`".format(message));
+      });
+      ws.on('close', function()
+      { wsClients.remove(ws);
+      });
+      // store websocket for broadcasting to it.
+      wsClients.add(ws);
+    }
+  });
 });
 
-// START SERVER
-server.listen(process.env.PORT || settings['port']);
+database.clearAll().then(function()
+{ return database.createRoom('null');
+}).then(function()
+{ // START SERVER
+  var port = process.env.PORT || settings['port'];
+  console.log("starting server on port {0}".format(port));
+  server.listen(port);
+}).catch(std_catch());
